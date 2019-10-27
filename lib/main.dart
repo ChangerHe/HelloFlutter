ea8eb8a2385d3de9a4cd23e29679622779049052
ChangerHe <244737162@qq.com>
2019-10-27 13:49:02 +0800

    feat: initial i10n

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:hello_flutter/i10n/localization_intl.dart';
 import 'package:hello_flutter/pages/index_route/index_route.dart';
 import 'package:hello_flutter/pages/page_splash_screen/page_splash_screen.dart';
 import 'package:hello_flutter/pages/panel_examples/panel_examples.dart';
@@ -12,6 +14,12 @@ void main() => runApp(
 //        theme: ThemeData.dark(),
         // 去除debug的图标
         debugShowCheckedModeBanner: false,
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          // 在此处额外设置一个本地化的delegate
+          DemoLocalizationsDelegate()
+        ],
         routes: {
           // "/": (context) => PanelIndex(),
           // 如果需要传参, 则在此处定义好入参的内容, 并传递到组件中
--- lib/pages/index_route/index_route.dart
+++ lib/pages/index_route/index_route.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:hello_flutter/i10n/localization_intl.dart';
 
 import 'package:hello_flutter/pages/panel_cart/panel_cart.dart';
 import 'package:hello_flutter/pages/panel_categories/panel_categories.dart';
@@ -8,8 +9,7 @@ import 'package:hello_flutter/pages/panel_index/panel_index.dart';
 import 'package:hello_flutter/pages/panel_member/panel_member.dart';
 
 final List<BottomNavigationBarItem> bottomTabs = [
-  BottomNavigationBarItem(
-      icon: Icon(CupertinoIcons.folder), title: Text('案例')),
+  BottomNavigationBarItem(icon: Icon(CupertinoIcons.folder), title: Text('案例')),
   BottomNavigationBarItem(icon: Icon(CupertinoIcons.search), title: Text('分类')),
   BottomNavigationBarItem(
       icon: Icon(CupertinoIcons.shopping_cart), title: Text('购物车')),
@@ -18,13 +18,7 @@ final List<BottomNavigationBarItem> bottomTabs = [
   BottomNavigationBarItem(icon: Icon(CupertinoIcons.home), title: Text('首页')),
 ];
 
-final List<String> tabTitles = [
-  '案例',
-  '分类',
-  '购物车',
-  '会员中心',
-  '首页'
-];
+final List<String> tabTitles = ['案例', '分类', '购物车', '会员中心', '首页'];
 
 class IndexRoutesPage extends StatefulWidget {
   _IndexRoutesPageState createState() => _IndexRoutesPageState();
@@ -35,37 +29,45 @@ class _IndexRoutesPageState extends State<IndexRoutesPage> {
   List<Widget> _pageList = List();
   var _activeColor = Colors.blueAccent;
   var _inactiveColor = Colors.grey;
+  String _title = '';
 
+  @override
   _IndexRoutesPageState() {
-    _pageList..addAll([
-      PanelExamples(),
-      PanelCategories(),
-      PanelCart(),
-      PanelMember(),
-      PanelIndex(),
-    ]);
+    _pageList
+      ..addAll([
+        PanelExamples(),
+        PanelCategories(),
+        PanelCart(),
+        PanelMember(),
+        PanelIndex(),
+      ]);
     // _pageList..add(value)
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(tabTitles[_activeTab]),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: _activeColor,
-        unselectedItemColor: _inactiveColor,
-        showUnselectedLabels: true,
-        currentIndex: _activeTab,
-        items: bottomTabs,
-        onTap: (index) {
-          setState(() {
-            _activeTab = index;
-          });
-        },
-      ),
-      body: _pageList[_activeTab]
-    );
+        appBar: AppBar(
+          // title: Text(tabTitles[_activeTab]),
+          title: Text(DemoLocalizations.of(context).title)
+          // title: Builder(
+          //   builder: (context) {
+          //     return Text(DemoLocalizations.of(context).title);
+          //   },
+          // ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: _activeColor,
+          unselectedItemColor: _inactiveColor,
+          showUnselectedLabels: true,
+          currentIndex: _activeTab,
+          items: bottomTabs,
+          onTap: (index) {
+            setState(() {
+              _activeTab = index;
+            });
+          },
+        ),
+        body: _pageList[_activeTab]);
   }
 }
